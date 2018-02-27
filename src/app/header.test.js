216ed0feda10b043b6f5ad4e57ce84eb6e45b5b8
Natcha Luangaroonchai <me@nomkhonwaan.com>
2018-02-27 11:51:20 +0700

    app-header component test

---
+++ src/app/header.test.js
@@ -0,0 +1,33 @@
+/**
+ * External Dependencies
+ */
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { spy } from 'sinon';
+
+/**
+ * Internal Dependencies
+ */
+import { Header } from './header';
+
+describe('app-header', () => {
+
+  describe('sidebar-toggle', () => {
+    describe('When user click on the sidebar-toggle button', () => {
+      it('should call `toggleSidebar` function', () => {
+        const toggleSidebar = spy();
+        const wrapper = shallow(
+          <Header
+            app={ { sidebar: { collapsed: true } } }
+            toggleSidebar={ toggleSidebar }
+          />
+        );
+
+        wrapper.find('.sidebar-toggle').simulate('click');
+
+        expect(toggleSidebar.calledOnce).toBe(true);
+      });
+    });
+  });
+  
+});
